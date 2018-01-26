061c4fa1b537d4d561776855a96260161e4fa27d
Adrien Poly <adrienpoly@gmail.com>
2018-01-26 13:09:27 +0100

    adds the image index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import { autoload } from "stimulus/webpack-helpers";
 import Turbolinks from "turbolinks";
 import Rails from "rails-ujs";
 import "./stylesheets/todomvc.scss";
+import "../images/index";
 const application = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
 
