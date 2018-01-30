343be62f0cb7dfb02c6d738705b42c03aa637b81
Adrien Poly <adrienpoly@gmail.com>
2018-01-30 23:30:56 +0100

    update stimulus editable controller for new api

--- app/javascript/controllers/editable_controller.js
+++ app/javascript/controllers/editable_controller.js
@@ -2,22 +2,23 @@ import { Controller } from "stimulus";
 import Rails from "rails-ujs";
 
 export default class extends Controller {
-  submitOnBlur(_event, target) {
-    Rails.fire(target, "submit");
+  submitOnBlur(event) {
+    const form = event.target.closest("form");
+    Rails.fire(form, "submit");
     target.closest("li").classList.remove("editing");
     this.targets.find("input-form").style.display = "none";
   }
 
-  closeOnEsc(event, target) {
-    if (event.keyCode === 27) {
-      target.blur();
-      target.closest("li").classList.remove("editing");
+  closeOnEsc(event) {
+    if (event.target.keyCode === 27) {
+      event.target.blur();
+      event.target.closest("li").classList.remove("editing");
       this.targets.find("input-form").style.display = "none";
     }
   }
 
-  editOnDbClick(event, target) {
-    target.closest("li").classList.add("editing");
+  editOnDbClick(event) {
+    event.target.closest("li").classList.add("editing");
     this.targets.find("input-form").style.display = "block";
     this.targets.find("input").focus();
   }
